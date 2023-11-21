f74d96a595cf7aff6dd20a54af3367d787f7d668
AGUIAtNight <72181488+AGUIAtNight@users.noreply.github.com>
2023-11-21 18:44:50 +0800

    Add files via upload

---
+++ out/Cameracapture.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'Cameracapture.exe' with Python DLL being found.
+set PATH=f:\fxxk\python38;%PATH%
+set PYTHONHOME=F:\FXXK\Python38
+"%~dp0.\Cameracapture.exe" %*
